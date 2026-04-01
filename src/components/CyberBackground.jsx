32f8641caecd33def9ea267bc6b9c5635087bf55
Camilo <camilox168@outlook.com>
2026-04-01 17:00:43 -0500

    Brightness changes to 0.40

--- src/components/CyberBackground.jsx
+++ src/components/CyberBackground.jsx
@@ -160,7 +160,7 @@ export default function CyberBackground() {
         style={{
           backgroundImage: "url('/img/bg.jpg')",
           opacity: 0.20,
-          filter: "brightness(0.60)"
+          filter: "brightness(0.40)"
         }}
       />
       <canvas
